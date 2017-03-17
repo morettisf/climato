2f74b0555b3b88ae8242a2bdff8eb3c11e6464fb
morettisf <morettisf@gmail.com>
2017-03-16 20:55:00 -0700

    searchbar work

--- src/containers/searchbar.js
+++ src/containers/searchbar.js
@@ -2,10 +2,37 @@ import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+
+  }
+
+  onInputChange(event) {
+    this.setState({ term: event.target.value });
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
+
+    this.setState({ term: '' });
+  }
+
   render() {
     return (
       <form className='input-group'>
-        <input />
+        <input 
+          placeholder='Search for your location'
+          className='form-control'
+          value={this.state.term}
+          onChange={this.onInputChange} />
+        <div className='input-group-btn'>
+          <button type='submit' className='btn btn-secondary'>Submit</button>
+        </div>
       </form>
     );
   }
