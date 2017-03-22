1e7e053a1d0423766ff22fe4a80b99a8fd74a3f8
morettisf <morettisf@gmail.com>
2017-03-22 13:18:54 -0700

    API url

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '99432abb3ddeecb498442945d0d3366b';
-const WEATHER_URL = 'http://api.openweathermap.org/data/2.5/forecast/daily?'; // 7 day forecast
+const WEATHER_URL = '//api.openweathermap.org/data/2.5/forecast/daily?'; // 7 day forecast
 
 export const GET_WEATHER = 'GET_WEATHER';
 
