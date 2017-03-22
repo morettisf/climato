8c1f5446a00564de4e26f8e6dc3414585238b4ba
morettisf <morettisf@gmail.com>
2017-03-22 12:13:36 -0700

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     loaders: [{
       test: /\.jsx$/,
       exclude: /node_modules/,
-      loader: 'babel',
+      loader: 'babel-loader',
       query: {
         presets: ['react', 'es2015', 'stage-0']
       }
