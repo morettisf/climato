37227ca4f0ef0b4a744e302b9da8306b8ad6695d
morettisf <morettisf@gmail.com>
2017-04-04 23:04:54 -0700

    removed navigator.geolocation

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -14,14 +14,13 @@ export class SearchBar extends Component {
     // binding these to the component's state
     this.onInputChange = this.onInputChange.bind(this);
     this.onFormSubmit = this.onFormSubmit.bind(this);
-    this.convertAddress = this.convertAddress.bind(this);
+    // this.convertAddress = this.convertAddress.bind(this);
   }
 
   componentWillMount() {
-    // prompt user to geolocate
-    navigator.geolocation.getCurrentPosition(function(position) {
-      this.convertAddress(position.coords.latitude, position.coords.longitude);
-    }.bind(this));
+    // navigator.geolocation.getCurrentPosition(function(position) {
+    //   this.convertAddress(position.coords.latitude, position.coords.longitude);
+    // }.bind(this));
 
     window.initAutocomplete = function() {
       const autocomplete = new google.maps.places.Autocomplete(document.getElementById('autocomplete'), 
@@ -29,14 +28,14 @@ export class SearchBar extends Component {
     }
   }
 
-  convertAddress(lat, lon) {
-    // geocoding user's lat & lon to a location that's searchable
-    axios.get('https://maps.googleapis.com/maps/api/geocode/json?latlng=' + lat + ',' + lon + 
-      '&key=AIzaSyAi3rCykF43m8bXF0A9TD9qSnPf_VMUW8c')
-      .then((res) => {
-        this.setState({term: res.data.results[1].formatted_address})
-      })
-  }
+  // convertAddress(lat, lon) {
+  //   // geocoding user's lat & lon to a location that's searchable
+  //   axios.get('https://maps.googleapis.com/maps/api/geocode/json?latlng=' + lat + ',' + lon + 
+  //     '&key=AIzaSyAi3rCykF43m8bXF0A9TD9qSnPf_VMUW8c')
+  //     .then((res) => {
+  //       this.setState({term: res.data.results[1].formatted_address})
+  //     })
+  // }
 
   onInputChange(event) {
     this.setState({ term: event.target.value });
